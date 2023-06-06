fdbdab8cecd2b89c658853534ef91b1c29c4b84d
Peter Benjamin <petermbenjamin@gmail.com>
2023-06-06 10:45:57 -0700

    chore: remove old recipes

--- makefiles/stow.mk
+++ makefiles/stow.mk
@@ -1,6 +1,6 @@
 # vim:ft=make:
 .PHONY: stow
-stow: stow-git stow-tmux stow-bash stow-vim ## Symlink all dot files
+stow: stow-git stow-tmux stow-bash ## Symlink all dot files
 
 .PHONY: stow-git
 stow-git: ## stow git
@@ -13,11 +13,3 @@ stow-tmux: ## stow tmux
 .PHONY: stow-bash
 stow-bash: ## stow bash
 	stow bash
-
-.PHONY: stow-vim
-stow-vim: ## stow git
-	stow vim
-
-.PHONY: stow-starship
-stow-starship: ## stow starship
-	stow starship
