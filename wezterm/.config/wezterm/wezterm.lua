fde95d97771c0653c2788c9eddfc7b7c0216ac94
Peter Benjamin <petermbenjamin@gmail.com>
2023-09-12 11:55:31 -0700

    feat(wezterm): add wezterm configuration

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+-- local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'tokyonight_storm'
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
