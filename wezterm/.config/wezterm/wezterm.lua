ca6e93228b735f0823cce7dce74d61d596d67e1b
Peter Benjamin <petermbenjamin@gmail.com>
2023-09-12 17:44:04 -0700

    chore: remove wezterm, reindent git config

--- wezterm/.config/wezterm/wezterm.lua
+++
@@ -1,8 +0,0 @@
--- local wezterm = require 'wezterm'
-local config = {}
-
-config.color_scheme = 'tokyonight_storm'
-config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
-
-return config
