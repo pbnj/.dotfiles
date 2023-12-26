1a1215251946fee102cdfd9905e925a031b64bbb
Peter Benjamin <petermbenjamin@gmail.com>
2023-12-26 14:21:27 -0800

    fix(wezterm): set color_scheme to tokyo night moon

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = "Tokyo Night Storm"
+config.color_scheme = "Tokyo Night Moon"
 config.enable_scroll_bar = false
 config.font = wezterm.font("JetBrains Mono")
 config.hide_tab_bar_if_only_one_tab = true
