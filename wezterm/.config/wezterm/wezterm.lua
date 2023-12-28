1185dff6c0fbd4cccc7031c1626192481f089f0e
Peter Benjamin <petermbenjamin@gmail.com>
2023-12-27 20:10:04 -0800

    fix(wezterm): set font to firacode nerd font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = {}
 
 config.color_scheme = "Tokyo Night Moon"
 config.enable_scroll_bar = false
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("FiraCode Nerd Font")
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
