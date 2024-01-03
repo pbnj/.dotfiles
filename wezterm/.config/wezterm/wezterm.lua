80c038579f9fd117eb9ae654c045a4710897a0ae
Peter Benjamin <petermbenjamin@gmail.com>
2024-01-03 09:28:50 -0800

    feat(wezterm): add transparency and blur

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,5 +6,7 @@ config.enable_scroll_bar = false
 config.font = wezterm.font("FiraCode Nerd Font")
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.90
+config.macos_window_background_blur = 25
 
 return config
