1a90f65e00b561cf6bb14f8c1991264cd78a7b9c
Peter Benjamin <petermbenjamin@gmail.com>
2024-01-10 10:34:33 -0800

    fix(wezterm): revert transparency

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,5 @@ config.enable_scroll_bar = false
 config.font = wezterm.font("FiraCode Nerd Font")
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.90
-config.macos_window_background_blur = 25
 
 return config
