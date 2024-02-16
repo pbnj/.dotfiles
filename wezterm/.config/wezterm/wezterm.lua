99e36cb77355cb95553bd95f7743663b3c0c7656
Peter Benjamin <petermbenjamin@gmail.com>
2024-02-15 16:30:52 -0800

    chore(wezterm): minor changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,12 @@
 local wezterm = require("wezterm")
-local config = {}
 
-config.color_scheme = "Tokyo Night Moon"
-config.enable_scroll_bar = false
-config.font = wezterm.font("FiraCode Nerd Font")
-config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
-
-return config
+return {
+  color_scheme = "Tokyo Night Moon",
+  enable_scroll_bar = false,
+  font = wezterm.font("Iosevka Nerd Font"),
+  font_size = 14.0,
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "RESIZE",
+  -- window_background_opacity = 0.95,
+  -- macos_window_background_blur = 25,
+}
