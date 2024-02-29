cf0a3a412ad8c43881f1c34df2e414568f40ded6
Peter Benjamin <petermbenjamin@gmail.com>
2024-02-28 17:35:05 -0800

    fix: various fixes and improvements

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,6 +7,16 @@ return {
   font_size = 14.0,
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "RESIZE",
-  -- window_background_opacity = 0.95,
-  -- macos_window_background_blur = 25,
+  keys = {
+    {
+      key = "w",
+      mods = "CMD",
+      action = wezterm.action.CloseCurrentPane({ confirm = true }),
+    },
+    {
+      key = "w",
+      mods = "CMD",
+      action = wezterm.action.CloseCurrentTab({ confirm = true }),
+    },
+  },
 }
