922584935bd7d7d8e2ea7ade7070cf46d08cdcdf
Peter Benjamin <petermbenjamin@gmail.com>
2024-05-08 12:09:22 -0700

    chore(brew): update brews

--- brew.sh
+++ brew.sh
@@ -1,41 +1,62 @@
 # shellcheck shell=bash
 # vim: set ft=bash
-brew install --cask 1password                 # password manager
-brew install --cask 1password-cli             # password manager
-brew install --cask docker                    # containers
-brew install --cask firefox                   # browser
-brew install --cask keycastr                  # show keypresses
-brew install --cask maccy                     # macos clipboard manager
-brew install --cask minecraft                 # ;)
-brew install --cask rectangle                 # window manager
-brew install --cask slack                     # chat
-brew install --cask spotify                   # music
-brew install --cask wezterm                   # terminal
-brew install bash                             # newer version of bash
-brew install bash-completion                  # completion for common tools & utilities
-brew install coreutils                        # core utilities
-brew install curl                             # curl
-brew install ddgr                             # duckduckgo cli
-brew install direnv                           # auto source .env files
-brew install docker                           # docker cli
-brew install fzf                              # fuzzy finder
-brew install git                              # git cli
-brew install git-extras                       # extra git utils
-brew install grep                             # newer version of grep
-brew install grex                             # regex generator
+
+# casks / binaries
+
+brew install --cask 1password     # password manager
+brew install --cask 1password-cli # password manager
+brew install --cask docker        # containers
+brew install --cask firefox       # browser
+brew install --cask arc           # browser
+brew install --cask maccy         # macos clipboard manager
+brew install --cask rectangle     # window manager
+brew install --cask slack         # chat
+brew install --cask spotify       # music
+brew install --cask alacritty     # terminal
+brew install --cask macvim        # vim gui
+
+# essentials
+
+brew install bash            # newer version of bash
+brew install bash-completion # completion for common tools & utilities
+brew install coreutils       # core utilities
+brew install curl            # curl
+brew install ddgr            # duckduckgo cli
+brew install direnv          # auto source .env files
+brew install docker          # docker cli
+brew install fzf             # fuzzy finder
+brew install gh              # github cli
+brew install git             # git cli
+brew install git-extras      # extra git utils
+brew install grep            # newer version of grep
+brew install pre-commit      # git hook manager
+brew install universal-ctags # ctags
+
+# language/framework specific utilities
+
+brew install gitleaks                         # secret scanner
 brew install hadolint                         # docker linter
 brew install helm                             # cli for helm
 brew install instrumenta/instrumenta/conftest # configuration file tester using opa
+brew install jq                               # cli processor for json
 brew install kubernetes-cli                   # cli for k8s
-brew install melody                           # regex generator
-brew install neovim                           # neovim
 brew install nmap                             # network mapper
 brew install pandoc                           # universal document processor
 brew install ripgrep                          # better grep
+brew install shellcheck                       # shell/bash linter
+brew install shfmt                            # shell/bash formatter
 brew install slides                           # terminal presentation tool
+brew install snyk                             # security scanner
 brew install stow                             # symlink farm manager
 brew install tfenv                            # terraform version manager
+brew install tflint                           # terraform linter
 brew install tmux                             # terminal multiplexer
-brew install tmux-mem-cpu-load                # utility for showing mem & cpu load
+brew install trivy                            # security scanner
+brew install trufflehog                       # secret scanner
 brew install vhs                              # terminal gif generator
-brew install vim
+
+# extras
+# brew install grex                             # regex generator
+# brew install melody                           # regex generator
+# brew install vim
+# brew install tmux-mem-cpu-load                # utility for showing mem & cpu load
