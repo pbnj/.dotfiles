7b7d952abd6d61921388f9604df499eec2d236ec
Peter Benjamin <petermbenjamin@gmail.com>
2024-08-28 08:12:42 -0700

    chore: remove old/unneeded configs

--- fish/.config/fish/completions/aws.fish
+++
@@ -1,5 +0,0 @@
-function __fish_complete_aws
-    env COMP_LINE=(commandline -pc) aws_completer | tr -d ' '
-end
-
-complete -c aws -f -a "(__fish_complete_aws)"
--- fish/.config/fish/config.fish
+++
@@ -1,4 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    bind \cy accept-autosuggestion execute
-end
