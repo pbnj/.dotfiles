3a9f33337a53c119755ef691865469277dc02f77
Peter Benjamin <petermbenjamin@gmail.com>
2025-04-22 10:21:56 -0700

    feat(neovim): run pin-github-action on github workflow files

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,7 @@
 return {
   "https://github.com/stevearc/conform.nvim",
-  event = "VeryLazy",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
   opts = {
     format_on_save = {
       timeout_ms = 500,
@@ -15,8 +16,40 @@ return {
       python = { "ruff_fix" },
       rust = { "rustfmt" },
       terraform = { "terraform_fmt" },
+      yaml = function(bufnr)
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match("/.github/workflows/") then
+          return { "pin_github_action", "prettierd" }
+        else
+          return { "prettierd" }
+        end
+      end,
     },
     formatters = {
+      injected = {
+        options = {
+          ignore_errors = true,
+          lang_to_ft = {
+            bash = "sh",
+          },
+          lang_to_ext = {
+            bash = "sh",
+            javascript = "js",
+            markdown = "md",
+            python = "py",
+            ruby = "rb",
+            rust = "rs",
+            shell = "sh",
+            terraform = "tf",
+            typescript = "ts",
+          },
+        },
+      },
+      pin_github_action = {
+        command = "pin-github-action",
+        args = { "$FILENAME" },
+        stdin = false,
+      },
       doctoc_update = {
         command = "doctoc",
         args = { "--update-only", "$FILENAME" },
