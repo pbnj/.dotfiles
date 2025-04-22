e61a6bf4fea3ff122bec44f94aba5b0deb7ef0c3
Peter Benjamin <petermbenjamin@gmail.com>
2025-04-22 10:23:25 -0700

    feat(neovim): add render-markdown and replace statusline with mini

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,16 @@
 return {
   {
+    "https://github.com/echasnovski/mini.nvim",
+    event = "VeryLazy",
+    version = false,
+    config = function()
+      require("mini.statusline").setup()
+      require("mini.icons").setup()
+    end,
+  },
+  {
     "https://github.com/nvim-lualine/lualine.nvim",
+    enabled = false,
     event = "VeryLazy",
     dependencies = { "https://github.com/nvim-tree/nvim-web-devicons" },
     opts = {
---
+++ neovim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+return {
+  "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "https://github.com/nvim-treesitter/nvim-treesitter", "https://github.com/echasnovski/mini.nvim" },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+}
