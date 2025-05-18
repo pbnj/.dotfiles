c9400df98ce10eb7ac2d64955c6591ddc244d6da
Peter Benjamin <petermbenjamin@gmail.com>
2025-05-18 10:37:37 -0700

    fix(neovim): remove noice & trouble

--- neovim/.config/nvim/lua/plugins/folke.lua
+++ neovim/.config/nvim/lua/plugins/folke.lua
@@ -1,45 +1,9 @@
 return {
-  {
-    "https://github.com/folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = { "https://github.com/MunifTanjim/nui.nvim" },
-    opts = {
-      -- lsp = {
-      --   override = {
-      --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      --     ["vim.lsp.util.stylize_markdown"] = true,
-      --   },
-      -- },
-      presets = { command_palette = true },
-    },
-  },
   -- {
-  --   "https://github.com/folke/trouble.nvim",
-  --   opts = {},
-  --   cmd = "Trouble",
-  --   keys = {
-  --     {
-  --       "<leader>xx",
-  --       function()
-  --         require("trouble").toggle({ mode = "diagnostics" })
-  --       end,
-  --       desc = "Toggle Diagnostics (Trouble)",
-  --     },
-  --     {
-  --       "]x",
-  --       function()
-  --         require("trouble").next({ mode = "diagnostics", jump = true })
-  --       end,
-  --       desc = "Next Diagnostics (Trouble)",
-  --     },
-  --     {
-  --       "[x",
-  --       function()
-  --         require("trouble").prev({ mode = "diagnostics", jump = true })
-  --       end,
-  --       desc = "Previous Diagnostics (Trouble)",
-  --     },
-  --   },
+  --   "https://github.com/folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = { "https://github.com/MunifTanjim/nui.nvim" },
+  --   opts = { presets = { command_palette = true } },
   -- },
   {
     "https://github.com/folke/snacks.nvim",
