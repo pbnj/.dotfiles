6b4783c56782adb7cccb7794306dc8d84797ba34
Peter Benjamin <petermbenjamin@gmail.com>
2025-06-12 08:23:37 -0700

    feat(neovim): implement AWSConsole command in lua

--- neovim/.config/nvim/plugin/aws.lua
+++ neovim/.config/nvim/plugin/aws.lua
@@ -1,28 +1,71 @@
-local function aws_profile_and_command_completion(a, l, _)
-  local args = vim.split(l, " ", { trimempty = true })
-  local last_arg = args[#args]
-  if last_arg == "--profile" then
-    -- profile completion
-    local profiles = vim.fn.systemlist("aws configure list-profiles")
-    return vim.tbl_filter(function(profile)
-      return string.match(profile, a)
-    end, profiles)
-  else
-    -- command completion
-    local aws_cmds = vim.fn.systemlist("cat ~/.aws/commands")
-    return vim.tbl_filter(function(aws_cmd)
-      return string.match(aws_cmd, a)
-    end, aws_cmds)
-  end
+local function aws_profile_completion(arglead)
+  return vim
+    .iter(vim.fn.systemlist("aws configure list-profiles"))
+    :filter(function(profile)
+      return string.match(profile, arglead)
+    end)
+    :totable()
 end
 
+-- local function aws_profile_and_command_completion(arglead, cmdline)
+--   local args = vim.split(cmdline, " ", { trimempty = true })
+--   local last_arg = args[#args]
+--   if last_arg == "--profile" then
+--     -- profile completion
+--     return aws_profile_completion(arglead)
+--   else
+--     -- command completion
+--     return vim
+--       .iter(vim.fn.systemlist("cat ~/.aws/commands"))
+--       :filter(function(aws_cmd)
+--         return string.match(aws_cmd, arglead)
+--       end)
+--       :totable()
+--   end
+-- end
+
+vim.api.nvim_create_user_command("AWSConsole", function(opts)
+  local aws_sso_start_url = vim.trim(vim.fn.system("grep 'sso_start_url' ~/.aws/config | uniq | awk -F'=' '{printf $2}'"))
+  local aws_profiles = vim
+    .iter(vim.fn.systemlist("aws configure list-profiles | grep -E '^\\d{12}'"))
+    :map(function(val)
+      return { text = val }
+    end)
+    :totable()
+  if opts.args == "" then
+    require("snacks").picker({
+      source = "aws_console",
+      layout = "vscode",
+      format = "text",
+      items = aws_profiles,
+      matcher = { fuzzy = true, frecency = true },
+      confirm = function(picker, item)
+        picker:close()
+        local selection = vim.split(item.text, "/")
+        local account_id = selection[1]
+        local role_name = selection[3]
+        local url = string.format("%s/console?account_id=%s&role_name=%s", aws_sso_start_url, account_id, role_name)
+        vim.system({ "open", url })
+      end,
+    })
+  else
+    local selection = vim.split(opts.args, "/")
+    local account_id = selection[1]
+    local role_name = selection[3]
+    local url = string.format("%s/console?account_id=%s&role_name=%s", aws_sso_start_url, account_id, role_name)
+    vim.system({ "open", url })
+  end
+end, {
+  nargs = "?",
+  complete = aws_profile_completion,
+})
+
 vim.api.nvim_create_user_command("AWS", function(opts)
-  local Terminal = require("toggleterm.terminal").Terminal
   local cmd = string.format("awe --no-cli-pager --cli-auto-prompt %s", opts.args)
-  Terminal:new({ cmd = cmd, close_on_exit = false, direction = "float" }):toggle()
+  require("snacks").terminal(cmd, { auto_close = false })
 end, {
   nargs = "*",
-  complete = aws_profile_and_command_completion,
+  complete = aws_profile_completion,
 })
 
 -- Register the AWSProfile command
@@ -32,5 +75,5 @@ vim.api.nvim_create_user_command("AWSProfile", function(opts)
   Terminal:new({ cmd = cmd, close_on_exit = false }):toggle()
 end, {
   nargs = "*",
-  complete = aws_profile_and_command_completion,
+  complete = aws_profile_completion,
 })
