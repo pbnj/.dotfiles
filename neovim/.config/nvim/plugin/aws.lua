ac5100e93c0ee83965d2611e6be8a060d0065290
Peter Benjamin <petermbenjamin@gmail.com>
2025-06-15 15:43:38 -0700

    feat(neovim): custom snacks picker for aws console

--- neovim/.config/nvim/plugin/aws.lua
+++ neovim/.config/nvim/plugin/aws.lua
@@ -7,38 +7,56 @@ local function aws_profile_completion(arglead)
     :totable()
 end
 
--- local function aws_profile_and_command_completion(arglead, cmdline)
---   local args = vim.split(cmdline, " ", { trimempty = true })
---   local last_arg = args[#args]
---   if last_arg == "--profile" then
---     -- profile completion
---     return aws_profile_completion(arglead)
---   else
---     -- command completion
---     return vim
---       .iter(vim.fn.systemlist("cat ~/.aws/commands"))
---       :filter(function(aws_cmd)
---         return string.match(aws_cmd, arglead)
---       end)
---       :totable()
---   end
--- end
-
 vim.api.nvim_create_user_command("AWSConsole", function(opts)
   local aws_sso_start_url = vim.trim(vim.fn.system("grep 'sso_start_url' ~/.aws/config | uniq | awk -F'=' '{printf $2}'"))
-  local aws_profiles = vim
-    .iter(vim.fn.systemlist("aws configure list-profiles | grep -E '^\\d{12}'"))
-    :map(function(val)
-      return { text = val }
-    end)
-    :totable()
   if opts.args == "" then
     require("snacks").picker({
       source = "aws_console",
       layout = "vscode",
-      format = "text",
-      items = aws_profiles,
+      finder = function()
+        return vim
+          .iter(vim.fn.systemlist("aws configure list-profiles | grep -E '^\\d{12}'"))
+          :map(function(profile)
+            local profile_split = vim.split(profile, "/")
+            local account_id = profile_split[1]
+            local account_alias = profile_split[2]
+            local role_name = profile_split[3]
+            local account_url = string.format("%s/console?account_id=%s&role_name=%s", aws_sso_start_url, account_id, role_name)
+            return {
+              text = profile,
+              item = profile,
+              account_id = account_id,
+              account_alias = account_alias,
+              account_url = account_url,
+            }
+          end)
+          :totable()
+      end,
+      format = function(item, _)
+        local a = Snacks.picker.util.align
+        local ret = {}
+        ret[#ret + 1] = { a(item.account_id, 12, { truncate = false }) }
+        ret[#ret + 1] = { "  " }
+        ret[#ret + 1] = { a(item.account_alias, 63, { truncate = true }) }
+        return ret
+      end,
       matcher = { fuzzy = true, frecency = true },
+      actions = {
+        yank_id = { action = "yank", field = "account_id", desc = "Yank ID" },
+        yank_alias = { action = "yank", field = "account_alias", desc = "Yank Alias" },
+        yank_profile = { action = "yank", desc = "Yank Profile" },
+        yank_url = { action = "yank", field = "account_url", desc = "Yank URL" },
+      },
+      win = {
+        input = {
+          keys = {
+            ["<m-i>"] = { "yank_id", mode = { "n", "i" } },
+            ["<m-a>"] = { "yank_alias", mode = { "n", "i" } },
+            ["<m-p>"] = { "yank_profile", mode = { "n", "i" } },
+            ["<m-u>"] = { "yank_url", mode = { "n", "i" } },
+          },
+        },
+      },
       confirm = function(picker, item)
         picker:close()
         local selection = vim.split(item.text, "/")
