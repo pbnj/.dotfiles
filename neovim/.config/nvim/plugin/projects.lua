d0a51ad018c23b191e56d1f90be5ec7215888293
Peter Benjamin <petermbenjamin@gmail.com>
2025-06-26 10:30:31 -0700

    feat(projects): implement custom project fuzzy picker

--- neovim/.config/nvim/plugin/projects.lua
+++ neovim/.config/nvim/plugin/projects.lua
@@ -1,5 +1,13 @@
 vim.api.nvim_create_user_command("Projects", function(opts)
-  Snacks.picker.files({ hidden = true, cwd = "~/Projects", exclude = { ".env" }, layout = { fullscreen = opts.bang } })
+  Snacks.picker.files({
+    title = "Projects",
+    hidden = true,
+    cwd = "~/Projects",
+    dirs = { "~/Projects" },
+    exclude = { ".env" },
+    layout = { fullscreen = opts.bang },
+    formatters = { file = { truncate = 100 } },
+  })
 end, { nargs = "*", bang = true, desc = "Projects Snacks Picker" })
 
 vim.keymap.set({ "n" }, "<leader>fp", vim.cmd.Projects, { desc = "[F]uzzy [P]rojects" })
