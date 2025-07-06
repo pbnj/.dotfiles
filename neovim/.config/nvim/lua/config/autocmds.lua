10ffb008911b8c4f5c1d4528dd7d74bd3f41c676
Peter Benjamin <petermbenjamin@gmail.com>
2025-07-06 15:23:31 -0700

    fix: various improvements

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -11,17 +11,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank({ higroup = "Visual", timeout = 300 })
   end,
 })
-
--- vim.api.nvim_create_autocmd("TermOpen", {
---   group = vim.api.nvim_create_augroup("terminal", { clear = true }),
---   pattern = "*",
---   command = "startinsert",
--- })
-
--- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
---   desc = "Display diagnostics floating window when hovering over issues"
---   group = vim.api.nvim_create_augroup("float_diagnostic_cursor", { clear = true }),
---   callback = function()
---     vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
---   end,
--- })
