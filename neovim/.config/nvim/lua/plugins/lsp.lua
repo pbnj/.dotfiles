6bf052055319e2af26b4e2a63e5d2813a96fb53a
Peter Benjamin <petermbenjamin@gmail.com>
2025-10-14 11:02:57 -0700

    refactor(lsp): move blink and copilot to lsp config

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,64 @@
 return {
   "https://github.com/neovim/nvim-lspconfig",
+  init = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+      callback = function(event)
+        local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
+        if client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+          vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = false }) -- :help lsp-completion
+        end
+        if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion) then
+          vim.lsp.inline_completion.enable(true) -- :help lsp-completion
+        end
+        local map = function(keys, func, desc, mode)
+          mode = mode or "n"
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
+        map("<c-s>", vim.lsp.buf.signature_help, "Signature Help", "i")
+        map("<c-space>", vim.lsp.completion.get, "Trigger completion suggestion", "i")
+      end,
+    })
+  end,
   event = "VeryLazy",
   dependencies = {
     { "https://github.com/b0o/SchemaStore.nvim" },
-    { "https://github.com/saghen/blink.cmp" },
+    {
+      "https://github.com/saghen/blink.cmp",
+      dependencies = { "https://github.com/rafamadriz/friendly-snippets" },
+      version = "1.*",
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+        keymap = {
+          ["<Tab>"] = {
+            "snippet_forward",
+            function() -- sidekick next edit suggestion
+              return require("sidekick").nes_jump_or_apply()
+            end,
+            function() -- if you are using Neovim's native inline completions
+              return vim.lsp.inline_completion.get()
+            end,
+            "fallback",
+          },
+        },
+        appearance = { nerd_font_variant = "mono" },
+        completion = { documentation = { auto_show = false }, list = { selection = { preselect = false, auto_insert = true } } },
+        sources = { default = { "lsp", "path", "snippets", "buffer" } },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+      },
+      opts_extend = { "sources.default" },
+    },
   },
   config = function()
-    vim.lsp.set_log_level("OFF")
-    local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities())
+    vim.lsp.log.set_level("OFF")
+    -- vim.lsp.set_log_level("OFF")
+    -- local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities())
     local servers = {
       -- misc
       docker_language_server = {},
       bashls = {},
-      snyk_language_server = {},
+      copilot = {},
       -- go
       gopls = {},
       golangci_lint_ls = {},
@@ -26,7 +72,6 @@ return {
       terraformls = {},
       tflint = {},
       -- config languages
-      regal = {},
       jsonls = {
         settings = {
           json = {
