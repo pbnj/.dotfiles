eac3c3dff1a231dba24c753c15fd5ff7e9670b42
Peter Benjamin <petermbenjamin@gmail.com>
2025-10-15 08:58:19 -0700

    fix(mini): enable mini.move

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -3,14 +3,15 @@ return {
   lazy = false,
   version = false,
   config = function()
-    require("mini.misc").setup_auto_root()
     require("mini.ai").setup({ n_lines = 500 })
+    require("mini.bracketed").setup()
     require("mini.diff").setup()
     require("mini.git").setup()
     require("mini.icons").setup()
+    require("mini.misc").setup_auto_root()
+    require("mini.move").setup()
+    require("mini.splitjoin").setup()
     require("mini.statusline").setup()
     require("mini.surround").setup()
-    require("mini.splitjoin").setup()
-    require("mini.bracketed").setup()
   end,
 }
