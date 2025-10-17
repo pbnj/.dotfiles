842f00fbc352ace2eeb331930833b1f660c34c87
Peter Benjamin <petermbenjamin@gmail.com>
2025-10-17 08:41:20 -0700

    chore(live-preview): minor config changes

--- neovim/.config/nvim/lua/plugins/live-preview.lua
+++ neovim/.config/nvim/lua/plugins/live-preview.lua
@@ -1,19 +1,26 @@
 return {
-  "https://github.com/selimacerbas/mermaid-playground.nvim",
-  dependencies = {
+  {
     "https://github.com/brianhuster/live-preview.nvim",
-    -- "https://github.com/selimacerbas/live-preview.nvim", -- TODO
+    dependencies = { "https://github.com/folke/snacks.nvim" },
+    opts = {},
+    ft = { "markdown" },
+    cmd = { "LivePreview" },
   },
-  opts = {
-    -- all optional; sane defaults shown
-    workspace_dir = ".mermaid-live", -- defaults to: $XDG_CONFIG_HOME/mermaid-playground
-    index_name = "index.html",
-    diagram_name = "diagram.mmd",
-    overwrite_index_on_start = false, -- don't clobber your customized index.html
-    auto_refresh = true,
-    auto_refresh_events = { "InsertLeave", "TextChanged", "TextChangedI", "BufWritePost" },
-    debounce_ms = 450,
-    notify_on_refresh = false,
+  {
+    "https://github.com/selimacerbas/mermaid-playground.nvim",
+    dependencies = { "https://github.com/selimacerbas/live-server.nvim" },
+    opts = {
+      -- all optional; sane defaults shown
+      workspace_dir = ".mermaid-live", -- defaults to: $XDG_CONFIG_HOME/mermaid-playground
+      index_name = "index.html",
+      diagram_name = "diagram.mmd",
+      overwrite_index_on_start = false, -- don't clobber your customized index.html
+      auto_refresh = true,
+      auto_refresh_events = { "InsertLeave", "TextChanged", "TextChangedI", "BufWritePost" },
+      debounce_ms = 450,
+      notify_on_refresh = false,
+    },
+    ft = { "markdown", "mermaid" },
+    cmd = { "MermaidPreviewStart" },
   },
-  ft = { "markdown", "mermaid" },
 }
