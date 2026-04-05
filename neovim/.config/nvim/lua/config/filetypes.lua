607cc02caa9183d705ec3c032dbca4c6fa01a7a3
Peter Benjamin <petermbenjamin@gmail.com>
2026-04-05 09:07:48 -0700

    fix(options): remove I from shortmess to show intro message

---
+++ neovim/.config/nvim/lua/config/filetypes.lua
@@ -0,0 +1,29 @@
+-- Filetypes
+vim.filetype.add({
+  extension = {
+    tofu = "terraform",
+    tf = "terraform",
+  },
+  filename = {
+    [".snyk"] = "yaml",
+    ["CODEOWNERS"] = "gitignore",
+    ["config.ghostty"] = "config",
+  },
+  pattern = {
+    --   [".*/%.github[%w/]+workflows[%w/]+.*%.ya?ml"] = "yaml.github",
+    [".*"] = {
+      function(_, bufnr)
+        local contents = vim.api.nvim_buf_get_lines(bufnr, 0, 3, false) or {}
+        for _, v in ipairs(contents) do
+          if v:match("apiVersion:%s%S+") then
+            return "yaml"
+          end
+        end
+        -- if vim.regex([[^apiVersion:]]):match_str(content) ~= nil then
+        --   return "yaml"
+        -- end
+      end,
+      { priority = -math.huge },
+    },
+  },
+})
--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,23 @@
--- Keymaps > General
+-- Keymaps
+
+-- Movements
 vim.keymap.set("c", "<C-n>", "<Down>", { noremap = true })
 vim.keymap.set("c", "<C-p>", "<Up>", { noremap = true })
 vim.keymap.set("n", "j", "gj", { noremap = true, silent = true })
 vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
+
+-- Search
 vim.keymap.set({ "n", "v" }, "n", function()
   return (vim.v.searchforward == 1 and "n" or "N")
 end, { expr = true, silent = true, desc = "Search forward" })
 vim.keymap.set({ "n", "v" }, "N", function()
   return (vim.v.searchforward == 1 and "N" or "n")
 end, { expr = true, silent = true, desc = "Search backward" })
+
+-- Plugin Manager
+vim.keymap.set("n", "<leader>pu", function()
+  vim.pack.update()
+end, { noremap = true, silent = true, desc = "[P]ack [U]pdate" })
+vim.keymap.set("n", "<leader>pg", function()
+  vim.pack.get()
+end, { noremap = true, silent = true, desc = "[P]ack [G]et" })
