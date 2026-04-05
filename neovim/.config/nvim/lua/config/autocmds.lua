8306e0781c40dbaa85a90873c39fda276a0aa90b
Peter Benjamin <petermbenjamin@gmail.com>
2026-04-05 09:58:36 -0700

    fix(autocmds): fix auto-toggle on macos by using osascript instead of defaults cli

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -26,22 +26,24 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Auto-toggle neovim background based on system theme
+local function get_system_theme()
+  if vim.loop.os_uname().sysname:match("Darwin") then
+    if vim.fn.systemlist({ "osascript", "-e", [[tell application "System Events" to tell appearance preferences to return dark mode]] })[1] == "true" then
+      return "dark"
+    else
+      return "light"
+    end
+  end
+end
+
 vim.api.nvim_create_autocmd("ColorScheme", {
   group = vim.api.nvim_create_augroup("colorscheme_change", { clear = true }),
   pattern = "*",
   callback = function()
     -- vim.api.nvim_set_hl(0, "Normal", { bg = nil })
-    vim.api.nvim_set_hl(0, "Visual", { link = "CursorLine" })
-    if vim.loop.os_uname().sysname:match("Darwin") then
-      if vim.fn.systemlist({ "defaults", "read", "-g", "AppleInterfaceStyle", "2>/dev/null" })[1]:match("Dark") then
-        vim.schedule(function()
-          vim.o.background = "dark"
-        end)
-      else
-        vim.schedule(function()
-          vim.o.background = "light"
-        end)
-      end
-    end
+    -- vim.api.nvim_set_hl(0, "Visual", { link = "CursorLine" })
+    vim.schedule(function()
+      vim.o.background = get_system_theme()
+    end)
   end,
 })
