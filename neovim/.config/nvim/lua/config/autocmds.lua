06cc9609026d196ba4b2266e965a876842c77e5b
Peter Benjamin <petermbenjamin@gmail.com>
2026-04-09 14:20:53 -0700

    feat(neovim): customize ui notifications

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -24,26 +24,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank({ timeout = 300 })
   end,
 })
-
--- Auto-toggle neovim background based on system theme
-local function get_system_theme()
-  if vim.loop.os_uname().sysname:match("Darwin") then
-    if vim.fn.systemlist({ "osascript", "-e", [[tell application "System Events" to tell appearance preferences to return dark mode]] })[1] == "true" then
-      return "dark"
-    else
-      return "light"
-    end
-  end
-end
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-  group = vim.api.nvim_create_augroup("colorscheme_change", { clear = true }),
-  pattern = "*",
-  callback = function()
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = nil })
-    vim.api.nvim_set_hl(0, "Visual", { link = "CursorLine" })
-    vim.schedule(function()
-      vim.o.background = get_system_theme()
-    end)
-  end,
-})
--- neovim/.config/nvim/lua/config/colorscheme.lua
+++ neovim/.config/nvim/lua/config/colorscheme.lua
@@ -1,2 +1,26 @@
 -- Colorscheme
+
+-- Autocmd to toggle neovim background based on system theme
+local function get_system_theme()
+  if vim.loop.os_uname().sysname:match("Darwin") then
+    if vim.fn.systemlist({ "osascript", "-e", [[tell application "System Events" to tell appearance preferences to return dark mode]] })[1] == "true" then
+      return "dark"
+    else
+      return "light"
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = vim.api.nvim_create_augroup("colorscheme_change", { clear = true }),
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = nil })
+    vim.api.nvim_set_hl(0, "Visual", { link = "CursorLine" })
+    vim.schedule(function()
+      vim.o.background = get_system_theme()
+    end)
+  end,
+})
+
 vim.cmd.colorscheme("default")
