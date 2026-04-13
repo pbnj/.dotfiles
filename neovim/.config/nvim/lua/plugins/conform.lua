c3cce1a37525ac141e275f849ef9be6057b9421e
Peter Benjamin <petermbenjamin@gmail.com>
2026-04-12 20:21:08 -0700

    fix(conform)!: disable conform.nvim due to performance issues

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -1,76 +1,76 @@
-vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
-
-require("conform").setup({
-  format_on_save = {
-    timeout_ms = 1500,
-    lsp_format = "fallback",
-  },
-  formatters_by_ft = {
-    bash = { "shellcheck", "shfmt" },
-    go = { "goimports", "golangci-lint" },
-    json = { "prettier" },
-    lua = { "stylua" },
-    markdown = { "injected", "markdownlint-cli2", "markdown-toc", "prettier", timeout_ms = 5000 },
-    python = { "ruff_format" },
-    rust = { "rustfmt" },
-    sh = { "shellcheck", "shfmt" },
-    sql = { "sqruff" },
-    terraform = { "terraform_fmt" },
-    yaml = function()
-      local bufname = vim.api.nvim_buf_get_name(0)
-      if bufname:match("/.github/workflows/") then
-        return { "pinact", "prettier" }
-      end
-      return { "prettier" }
-    end,
-  },
-  formatters = {
-    injected = {
-      options = {
-        ignore_errors = true,
-        lang_to_ft = {
-          bash = "sh",
-        },
-        lang_to_ext = {
-          bash = "sh",
-          javascript = "js",
-          markdown = "md",
-          python = "py",
-          ruby = "rb",
-          rust = "rs",
-          shell = "sh",
-          terraform = "tf",
-          typescript = "ts",
-        },
-      },
-    },
-    pinact = {
-      command = "pinact",
-      args = { "run", "$RELATIVE_FILEPATH" },
-      stdin = false,
-      env = {
-        GITHUB_TOKEN = vim.fn.systemlist("gh auth token")[1],
-      },
-    },
-    pin_github_action = {
-      command = "pin-github-action",
-      args = { "$RELATIVE_FILEPATH" },
-      stdin = false,
-    },
-    doctoc_update = {
-      command = "doctoc",
-      args = { "--update-only", "$FILENAME" },
-      stdin = false,
-    },
-    markdown_toc = {
-      command = "markdown-toc",
-      args = function(_, ctx)
-        local indent = vim.bo[ctx.buf].expandtab and (" "):rep(ctx.shiftwidth) or "\t"
-        return { "--no-firsth1", "--bullets", "-", "--indent=" .. indent, "-i", "$FILENAME" }
-      end,
-      stdin = false,
-    },
-  },
-})
-
-vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+-- vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
+--
+-- require("conform").setup({
+--   format_on_save = {
+--     timeout_ms = 1500,
+--     lsp_format = "fallback",
+--   },
+--   formatters_by_ft = {
+--     bash = { "shellcheck", "shfmt" },
+--     go = { "goimports", "golangci-lint" },
+--     json = { "prettier" },
+--     lua = { "stylua" },
+--     markdown = { "injected", "markdownlint-cli2", "markdown-toc", "prettier", timeout_ms = 5000 },
+--     python = { "ruff_format" },
+--     rust = { "rustfmt" },
+--     sh = { "shellcheck", "shfmt" },
+--     sql = { "sqruff" },
+--     terraform = { "terraform_fmt" },
+--     yaml = function()
+--       local bufname = vim.api.nvim_buf_get_name(0)
+--       if bufname:match("/.github/workflows/") then
+--         return { "pinact", "prettier" }
+--       end
+--       return { "prettier" }
+--     end,
+--   },
+--   formatters = {
+--     injected = {
+--       options = {
+--         ignore_errors = true,
+--         lang_to_ft = {
+--           bash = "sh",
+--         },
+--         lang_to_ext = {
+--           bash = "sh",
+--           javascript = "js",
+--           markdown = "md",
+--           python = "py",
+--           ruby = "rb",
+--           rust = "rs",
+--           shell = "sh",
+--           terraform = "tf",
+--           typescript = "ts",
+--         },
+--       },
+--     },
+--     pinact = {
+--       command = "pinact",
+--       args = { "run", "$RELATIVE_FILEPATH" },
+--       stdin = false,
+--       env = {
+--         GITHUB_TOKEN = vim.fn.systemlist("gh auth token")[1],
+--       },
+--     },
+--     pin_github_action = {
+--       command = "pin-github-action",
+--       args = { "$RELATIVE_FILEPATH" },
+--       stdin = false,
+--     },
+--     doctoc_update = {
+--       command = "doctoc",
+--       args = { "--update-only", "$FILENAME" },
+--       stdin = false,
+--     },
+--     markdown_toc = {
+--       command = "markdown-toc",
+--       args = function(_, ctx)
+--         local indent = vim.bo[ctx.buf].expandtab and (" "):rep(ctx.shiftwidth) or "\t"
+--         return { "--no-firsth1", "--bullets", "-", "--indent=" .. indent, "-i", "$FILENAME" }
+--       end,
+--       stdin = false,
+--     },
+--   },
+-- })
+--
+-- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
