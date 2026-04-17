e27db02007992af740df916249b3c9f50534b5b6
Peter Benjamin <petermbenjamin@gmail.com>
2026-04-17 08:22:55 -0700

    feat(neogit): add keymap for convenient access

--- neovim/.config/nvim/lua/plugins/neogit.lua
+++ neovim/.config/nvim/lua/plugins/neogit.lua
@@ -1,1 +1,2 @@
 vim.pack.add({"https://github.com/nvim-lua/plenary.nvim", "https://github.com/NeogitOrg/neogit"})
+vim.keymap.set({"n"}, "<leader>gg" ,function() require("neogit").open() end, { desc = "Neogit" })
