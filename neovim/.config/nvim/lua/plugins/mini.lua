f337fbb1f4847cffdb1cf392c07612d359b1c8c1
Peter Benjamin <petermbenjamin@gmail.com>
2026-04-18 07:31:40 -0700

    chore: delete inactive code

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,5 @@
 vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
 
--- require("mini.ai").setup({ n_lines = 500 })
 require("mini.bracketed").setup()
 require("mini.diff").setup()
 require("mini.git").setup()
