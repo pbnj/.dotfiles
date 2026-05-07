92bbf5de7d4c7a707736160e5ab3c0ec66acb648
Peter Benjamin <petermbenjamin@gmail.com>
2026-05-06 22:05:35 -0700

    feat: reinstall fugitive and some minor changes

--- neovim/.config/nvim/lua/plugins/neogit.lua
+++ neovim/.config/nvim/lua/plugins/neogit.lua
@@ -1,2 +1,2 @@
-vim.pack.add({"https://github.com/nvim-lua/plenary.nvim", "https://github.com/NeogitOrg/neogit"})
-vim.keymap.set({"n"}, "<leader>gg" ,function() require("neogit").open() end, { desc = "Neogit" })
+-- vim.pack.add({"https://github.com/nvim-lua/plenary.nvim", "https://github.com/NeogitOrg/neogit"})
+-- vim.keymap.set({"n"}, "<leader>gg" ,function() require("neogit").open() end, { desc = "Neogit" })
---
+++ neovim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -0,0 +1,2 @@
+vim.pack.add({ "https://github.com/tpope/vim-fugitive", "https://github.com/tpope/vim-rhubarb" })
+vim.keymap.set({"n"}, "<leader>gg" ,function() vim.cmd.Git() end, { desc = "[G]it Status (fugitive)" })
