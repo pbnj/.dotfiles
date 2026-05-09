56cee9fa6497b204aacca963fa76bca04ab886ad
Peter Benjamin <petermbenjamin@gmail.com>
2026-05-08 23:11:28 -0700

    chore(neovim): clean up

--- neovim/.config/nvim/lua/plugins/cloak.lua
+++ neovim/.config/nvim/lua/plugins/cloak.lua
@@ -1,9 +1,2 @@
--- return {
---   "https://github.com/laytan/cloak.nvim",
---   event = "VeryLazy",
---   opts = {},
---   cmd = { "CloakDisable", "CloakdEnable", "CloakToggle" },
--- }
-
 vim.pack.add({ "https://github.com/laytan/cloak.nvim" })
 require("cloak").setup()
