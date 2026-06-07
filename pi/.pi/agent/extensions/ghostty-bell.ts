d1cedbd7ee8b7d229d95a7014593382ab9da700e
Peter Benjamin <petermbenjamin@gmail.com>
2026-06-07 08:55:00 -0700

    chore: remove extension

--- pi/.pi/agent/extensions/ghostty-bell.ts
+++
@@ -1,7 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-export default function (pi: ExtensionAPI) {
-  pi.on("agent_end", async (_event, _ctx) => {
-    process.stdout.write("\x07");
-  });
-}
