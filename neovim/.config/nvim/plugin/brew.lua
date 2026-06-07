bfea1ca6c12e5081ac08e0df51d5837241b6fec7
Peter Benjamin <petermbenjamin@gmail.com>
2026-06-07 08:53:38 -0700

    chore: brew update in new tab

--- neovim/.config/nvim/plugin/brew.lua
+++ neovim/.config/nvim/plugin/brew.lua
@@ -1,4 +1,4 @@
 vim.keymap.set({ "t", "n", "i" }, "<c-\\><c-u>", function()
-  vim.cmd("botright new")
+  vim.cmd("tabnew")
   vim.fn.jobstart({ "pkg_up" }, { term = true })
 end, { desc = "Terminal: Update system packages" })
