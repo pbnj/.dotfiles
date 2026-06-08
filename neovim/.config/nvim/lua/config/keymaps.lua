e21dd5112da415719b730e4934296affe7b18ff2
Peter Benjamin <petermbenjamin@gmail.com>
2026-06-07 20:44:56 -0700

    fix(neovim): disable left/right scroll wheel mappings

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,9 @@
 -- Keymaps
 
+-- Disable mouse scroll
+vim.keymap.set({ "n", "i", "c" }, "<ScrollWheelLeft>", "<nop>")
+vim.keymap.set({ "n", "i", "c" }, "<ScrollWheelRight>", "<nop>")
+
 -- Movements
 vim.keymap.set("c", "<C-n>", "<Down>", { noremap = true })
 vim.keymap.set("c", "<C-p>", "<Up>", { noremap = true })
@@ -8,22 +12,22 @@ vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
 
 -- Search
 vim.keymap.set({ "n", "v" }, "n", function()
-	return (vim.v.searchforward == 1 and "n" or "N")
+  return (vim.v.searchforward == 1 and "n" or "N")
 end, { expr = true, silent = true, desc = "Search forward" })
 vim.keymap.set({ "n", "v" }, "N", function()
-	return (vim.v.searchforward == 1 and "N" or "n")
+  return (vim.v.searchforward == 1 and "N" or "n")
 end, { expr = true, silent = true, desc = "Search backward" })
 
 -- Plugin Manager
 vim.keymap.set("n", "<leader>pp", function()
-	vim.pack.update()
+  vim.pack.update()
 end, { noremap = true, silent = true, desc = "[P]ack: Update [P]lugins" })
 
 -- Format
 vim.keymap.set("n", "Q", function()
-	if vim.bo.formatexpr ~= "" then
-		return "gggqG"
-	else
-		return ""
-	end
+  if vim.bo.formatexpr ~= "" then
+    return "gggqG"
+  else
+    return ""
+  end
 end, { noremap = true, silent = true, desc = "Format buffer" })
