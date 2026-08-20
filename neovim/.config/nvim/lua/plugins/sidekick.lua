d4f4d304f6e46a31be006eb9558d2c809eb3edaf
Peter Benjamin <petermbenjamin@gmail.com>
2026-08-20 13:07:35 -0400

    chore(config): update terminal and editor settings

--- neovim/.config/nvim/lua/plugins/sidekick.lua
+++ neovim/.config/nvim/lua/plugins/sidekick.lua
@@ -3,6 +3,7 @@ vim.pack.add({ "https://github.com/folke/sidekick.nvim" })
 require("sidekick").setup({
   nes = { enabled = false },
   cli = {
+    win = { layout = "bottom" },
     tools = {
       copilot = {
         cmd = { "copilot", "--yolo" },
