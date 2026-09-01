8dc62ffc734d0ebae20f8395cb23e197df36dad3
Peter Benjamin <petermbenjamin@gmail.com>
2026-09-01 14:39:05 -0400

    feat(neovim): add cloak toggle keymaps
    Cloaking is only useful when it can be lifted: reading a masked value out of a
    .env means either disabling the plugin or leaving the buffer. <leader>tc
    toggles it globally, <leader>tC reveals just the line under the cursor.

--- neovim/.config/nvim/lua/plugins/cloak.lua
+++ neovim/.config/nvim/lua/plugins/cloak.lua
@@ -1,2 +1,15 @@
 vim.pack.add({ "https://github.com/laytan/cloak.nvim" })
 require("cloak").setup()
+
+vim.keymap.set(
+  "n",
+  "<leader>tc",
+  "<cmd>CloakToggle<cr>",
+  { desc = "[T]oggle [C]loak" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>tC",
+  "<cmd>CloakPreviewLine<cr>",
+  { desc = "[T]oggle [C]loak (current line)" }
+)
